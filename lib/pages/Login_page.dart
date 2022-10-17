42c467e9a1a29ab93572bd616f06eba19be2226a
idealsuraj <youremail@yourdomain.com>
2022-10-17 17:14:47 +0545

    dsf

--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -3,23 +3,43 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    var paint = Paint();
+    paint.color = Colors.black54;
+    paint.style = PaintingStyle.stroke;
+
     return Material(
       child: Center(
         child: Text(
-          "Login Page",
+          "Login Page. ",
           style: TextStyle(
-            fontSize: 20,
+
+            // fontSize: 20,
             color: Colors.blue,
+
             fontWeight: FontWeight.bold,
               shadows:[Shadow(color:Colors.red, offset:Offset(1,2), blurRadius: 4 ) ],
-              fontStyle: FontStyle.normal,
-              decoration:TextDecoration.underline,
-              decorationColor: Colors.black54,
-              decorationStyle: TextDecorationStyle.wavy,
-              fontFamily: "alex"
+              // fontStyle: FontStyle.normal,
+              // decoration:TextDecoration.underline,
+              // decorationColor: Colors.black54,
+              // decorationStyle: TextDecorationStyle.wavy,
+              // fontFamily: "alex",
+              // fontStyle: FontStyle.normal,
+              background: paint,
+              // wordSpacing: 10,
+              letterSpacing: 5
+              // background: new Paint()
+              //   ..color = Colors.red
+              //   ..style = PaintingStyle.stroke
+              //   ..strokeWidth = 4,
           ),
           textScaleFactor: 2.0,
+
+
           textAlign: TextAlign.center ,
+          // maxLines: 1,
+          // softWrap: true,
+          // overflow: TextOverflow.ellipsis,
         ),
       ),
     );
